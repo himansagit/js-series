b9c97ef3de61d4992914d41f57fa8c636adf8dc9
Himanshu Singh <singhh208@gmail.com>
2025-04-11 20:56:29 +0530

    01_basics complete

---
+++ 01_baiscs/06_numsAndMaths.js
@@ -0,0 +1,17 @@
+// const score = 400
+
+// const balance = new Number(100)
+
+// console.log(score)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 32123.14
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+// -------------Maths-----------------------//
+
+console.log(Math.random())
---
+++ 01_baiscs/07_dateAndTime.js
@@ -0,0 +1,22 @@
+// jan 1st 1970 -> starting point of count
+//Dates
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+ let myCreatedDate = new Date(2025,3,11,5,3)
+//let myCreatedDate = new Date("2025-04-11")
+//console.log(myCreatedDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
